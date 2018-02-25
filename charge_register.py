9a6a5f08b23f9a360ebb1a26769bfe7918fbde79
André Leite <pelommedrado@gmail.com>
2018-02-25 19:40:03 -0300

    correcao da capacidade da bateria

--- charge_register.py
+++ charge_register.py
@@ -10,5 +10,9 @@ database = Database.get_instance()
 port = config.get('usb')[0]
 tracer_service = TracerService(serialclient=None, port=port)
 
-service = ChargeEquipmentService(tracer_service=tracer_service, database=database)
-print service.register()
+# service = ChargeEquipmentService(tracer_service=tracer_service, database=database)
+# print service.register()
+
+tracer_service.read_value('Battery Capacity')
+tracer_service.write_value('Battery Capacity', 220)
+print tracer_service.read_value('Battery Capacity')
