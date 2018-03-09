0878f59996fb1d2e87a81b8685c672e7f67fb963
André Leite <pelommedrado@gmail.com>
2018-03-08 23:21:15 -0300

    melhorias e ajustes

--- service/sampleService.py
+++ service/sampleService.py
@@ -9,7 +9,7 @@ from env import config
 from entity.sample import Sample
 from entity.sampleStatistical import SampleStatistical
 
-from sqlalchemy import func, desc
+from sqlalchemy import func, desc, hour
 
 class SampleService(object):
     def __init__(self, tracer_service=None, database=None):
@@ -165,7 +165,7 @@ class SampleService(object):
         session = self.database.create_session()
         query = session.query(func.avg(Sample.VoltageBattery))
         query = query.order_by(desc(Sample.CreatedDate))
-        query = query.group_by(func.hour(Sample.CreatedDate))
+        query = query.group_by(hour(Sample.CreatedDate))
         query = query.limit(1)
         sampleHour = query.all()
 
