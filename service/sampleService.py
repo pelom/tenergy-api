f0e359f7150daaca487a624022b1886186103fb9
André Leite <pelommedrado@gmail.com>
2018-03-16 23:56:34 -0300

    melhorias no calc paineis

--- service/sampleService.py
+++ service/sampleService.py
@@ -226,7 +226,7 @@ class SampleService(object):
                               func.avg(Sample.CurrentDischarging), func.max(Sample.CurrentDischarging), func.min(Sample.CurrentDischarging),
                               func.avg(Sample.VoltageDischarging), func.max(Sample.VoltageDischarging), func.min(Sample.VoltageDischarging)
                               )
-        query = query.filter(Sample.CurrentDischarging > 0, Sample.PowerLowDischarging > 0, Sample.VoltageDischarging > 0,
+        query = query.filter(Sample.CurrentDischarging > 0, Sample.PowerLowDischarging > 0,
                              Sample.CreatedDate >= start_date, Sample.CreatedDate < end_date)
         load = query.first()
 
